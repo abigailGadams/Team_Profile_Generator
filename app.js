7cd382734e1b5b8cade7954581ab9290a544a253
Abigail Adams <abigail-adams@gmail.com>
2021-05-21 00:37:51 -0400

    adding html generator

--- app.js
+++ app.js
@@ -1,19 +1,142 @@
 // Allows to selectively choose which modules we need
 const fs = require("fs");
-
+const inquirer = require("inquirer");
+const Employee = require("./library/Employee");
+const Engineer = require("./library/Engineer");
+const Intern = require("./library/Intern");
+const Manager = require("./library/Manager");
 // In the destination file that we want to receive exported function ** rel path must be exact
-const generatePage = require("./src/page-template.js");
+const generatePage = require("./src/createHTML.js");
 
-// Collecting command line arguments to feed into generateog function
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const teamArr = [];
 
-// Extract data and put into distinct arguments -> const name = profileDataArgs[0]; const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
+function getManagerInfo() {
+  const questions = [
+    {
+      type: "input",
+      message: "What is the name of your team manager?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the Manager's office number?",
+      name: "number",
+    },
+    {
+      type: "input",
+      message: "What is the employee ID?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is the email address?",
+      name: "email",
+    },
+  ];
 
+  inquirer.prompt(questions).then(function (data) {
+    const manager = new Manager(data.name, data.id, data.email, data.number);
+    teamArr.push(manager);
+    addTeamMember();
+  });
+}
+
+function addTeamMember() {
+  const questions = [
+    {
+      type: "list",
+      message: "Select the following team member you want to add",
+      choices: ["Engineer", "Intern", "EXIT"],
+      name: "teamMemberTitle",
+    },
+  ];
+  inquirer.prompt(questions).then(function (data) {
+    switch (data.teamMemberTitle) {
+      case "Engineer":
+        getEngineerInfo();
+        break;
+      case "Intern":
+        getInternInfo();
+
+        break;
+      default:
+        generateTeamManager();
+    }
+  });
+}
+
+function generateTeamManager() {
+  console.log(teamArr);
+  const teamManager = generatePage(teamArr);
+  fs.writeFileSync("./dist/team.html", teamManager);
+}
+
+function getInternInfo() {
+  const questions = [
+    {
+      type: "input",
+      message: "What is the name of your team intern?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the intern's ID?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is the email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is the intern's school?",
+      name: "school",
+    },
+  ];
+
+  inquirer.prompt(questions).then(function (data) {
+    const intern = new Intern(data.name, data.id, data.email, data.school);
+    teamArr.push(intern);
+    addTeamMember();
+  });
+}
+function getEngineerInfo() {
+  const questions = [
+    {
+      type: "input",
+      message: "What is the name of your team Engineer?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the engineer's ID?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is the engineer's email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is the engineer's github?",
+      name: "github",
+    },
+  ];
+
+  inquirer.prompt(questions).then(function (data) {
+    const engineer = new Engineer(data.name, data.id, data.email, data.github);
+    teamArr.push(engineer);
+    addTeamMember();
+  });
+}
+
+getManagerInfo();
 // Must contain 3 elements: name of file, data to write onto file(html), and callback f(n)
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  // display err msg if error occurs
-  if (err) throw new Error(err);
+// fs.writeFile("./index.html", generatePage(profileDataArgs), (err) => {
+//   // display err msg if error occurs
+//   if (err) throw new Error(err);
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
